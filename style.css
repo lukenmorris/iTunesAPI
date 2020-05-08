ae1373867433c3cc96359155a46e789ef423f53f
lukerock7 <56939157+lukerock7@users.noreply.github.com>
2020-05-08 13:33:53 -0700

    Create style.css

---
+++ style.css
@@ -0,0 +1,43 @@
+h1{
+    text-align: center;
+    color: darkorchid;
+    text-weight: bold;
+    font-size: 75px;
+    -webkit-text-stroke-color: cornsilk;
+    -webkit-text-stroke-width: 2px;
+    font-family: "American Typewriter";
+}
+
+#content{
+    position: relative;
+    margin: auto;
+    text-align: center;
+}
+body{
+    background-color: mistyrose;
+
+}
+/*!*Table Words*!*/
+#detailsListed {
+    text-align: center;
+    text-weight: bold;
+}
+
+/*Tables*/
+#resultTable {
+    position: relative;
+    margin: auto;
+    width: 50%;
+    background-color: cornsilk;
+}
+
+#searchButton, #pickArtist, #pickNumResults, #error {
+    background-color: darkorchid;
+    color: white;
+    text-align: center;
+}
+
+#error {
+    font-size: 50px;
+    font-family: "American Typewriter";
+}
