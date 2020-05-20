6982958beb325505f23c7e5abe047aaabe8433c7
lukerock7 <56939157+lukerock7@users.noreply.github.com>
2020-05-20 12:18:48 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,56 +1,48 @@
-   $('#searchButton').on('click', function(){
-        var artist = document.getElementById("pickArtist").value;
-        console.log(artist);
-        var numToDisplay = document.getElementById("pickNumResults").value;
-        console.log(numToDisplay);
+$(document).ready(function(){
 
-        $('#searchButton').animate('mouseenter',{color: 'cornsilk'});
-        $.ajax({
-            url: 'https://itunes.apple.com/search?limit=' + numToDisplay + '&term=' + artist,
-            type: 'GET',
-            crossDomain: true,
-            dataType: 'jsonp',
-            success: function(result) {
-                console.log(result);
-                myFunction(result, numToDisplay, artist)},
-            error: function() { alert('Failed!'); }
-        });
+    $("#search").click(function(){
+        var artist= $("#artist").val();
+
+
+         $.ajax({
+            url:"https://itunes.apple.com/search?term=" + musician,
+            dataType: "jsonp",
+            success: callBack,
+        })
     });
 
-});
 
-function myFunction(r, num, artist){
-    var itunesArray = r.results;
-    console.log(itunesArray);
 
-    if(artist != "selectArtist" && num != "selectNumResults"){
-        var result = "<table id='detailsListed' border=”1” cellpadding=15>";
-        result +="<tr>";
-        result+="<td>" + "Artist" + "</td>";
-        result+="<td>" + "Album" + "</td>";
-        result+="<td>" + "Album Cover" + "</td>";
-        result+="</tr>";
-        for (var i = 0; i < itunesArray.length; i++) {
-            result += "<tr>";
 
-            result += "<td>" + itunesArray[i].trackName + "</td>";
-            result += "<td>" + itunesArray[i].collectionName + "</td>";
-            result += "<td>" + "<img src=" + itunesArray[i].artworkUrl100 + "></td>";
+    function callBack(response) {
+        $("#list").empty();
+        console.log(response);
 
-            result += "</tr>";
-        }
-        result += "</table>"
-        document.getElementById("resultTable").innerHTML = result;
-        document.getElementById("resultTable").style.display = "block";
-    } else {
-        document.getElementById("resultTable").style.display = "none";
-        document.getElementById("error").innerHTML = "ERROR. Please select an artist and a number of results to display in order to search iTunes. Click here to exit.";
-        $("#error").click(function(){
-            $("#error").hide('fast');
-        });
-        $("#searchButton").click(function(){
-            $("#error").show('fast');
-        });
+        var table = "<table id='table'>";
+        var find = $("#number").val();
+
+        for (var i = 0; i < find; i++) {
+            var sound = "<audio controls= 'true' src=" + response.results[i].previewUrl + "></audio>";
+            var pic = "<img src=" + response.results[i].artworkUrl100 + ">";
+            var song = response.results[i].trackName;
+            var album = response.results[i].collectionName;
+            var ranknumber = response.results[i].trackNumber;
+
+
+
+
+            table += "<tr>";
+            table += "<td>" + sound + "</td>";
+            table += "<td>" + pic + "</td>";
+            table += "<td>" + "Song Title: " +song + "</td>";
+            table += "<td>" + "Album: " +album + "</td>";
+            table += "<td>" + "Rank: "+ ranknumber + "</td>";
+            table += "</tr>";
+            }
+            table += "</table>";
+
+            $("#list").append(table);
+
     }
 
-}
+});
